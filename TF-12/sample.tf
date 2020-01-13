e6d5bca9279e4ec824e90da831baf61fcdeba719
sudsahoo <59649124+sudsahoo@users.noreply.github.com>
2020-01-13 15:40:27 +0530

    Update sample.tf

--- TF-12/sample.tf
+++ TF-12/sample.tf
@@ -1,10 +1,18 @@
-variable "access_key" {}
-variable "credentials" {}
-variable "bucket_name" {}
-variable "gcp_project" {}
-
-
-resource "null_resource" "tf_instance" {
+variable "access_key" {
 }
-
-output "ak" {value="x"}
+variable "secret_key" {
+}
+variable "tag" {
+}
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.access_key
+  secret_key = var.secret_key
+}
+resource "aws_instance" "tf_instance" {
+  ami           = "ami-0922553b7b0369273"
+  instance_type = "t2.micro"
+  tags = {
+    Name = var.tag
+  }
+}
